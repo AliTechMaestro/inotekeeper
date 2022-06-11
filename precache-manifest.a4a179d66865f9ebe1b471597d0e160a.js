cb60dd9133d04d3a9374b2663025212d22222ca3
DELL PC <bsef19a525@pucit.edu.pk>
2022-06-11 16:48:47 +0500

    Updates

---
+++ precache-manifest.a4a179d66865f9ebe1b471597d0e160a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "167969706b9950ebdcbc",
+    "url": "/inotekeeper/static/css/main.aa6c31e2.chunk.css"
+  },
+  {
+    "revision": "167969706b9950ebdcbc",
+    "url": "/inotekeeper/static/js/main.16796970.chunk.js"
+  },
+  {
+    "revision": "7d8a5b69bced8be18895",
+    "url": "/inotekeeper/static/js/1.7d8a5b69.chunk.js"
+  },
+  {
+    "revision": "b529ba4ff608fc39bebd",
+    "url": "/inotekeeper/static/css/2.266488c0.chunk.css"
+  },
+  {
+    "revision": "b529ba4ff608fc39bebd",
+    "url": "/inotekeeper/static/js/2.b529ba4f.chunk.js"
+  },
+  {
+    "revision": "ed459cc420bd214703aa",
+    "url": "/inotekeeper/static/js/runtime~main.ed459cc4.js"
+  },
+  {
+    "revision": "ec25baea9936f5a1d446d18951049940",
+    "url": "/inotekeeper/index.html"
+  }
+];
